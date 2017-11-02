c9d84397e0ee4b42b2d1968d12e6ad10b9d0f56e
rahil dar <rahildar@rahils-MacBook-Air.local>
2017-11-02 22:44:31 +0530

    adding swagger to express apis

---
+++ node-swagger-api/routes/index.js
@@ -0,0 +1,54 @@
+var express = require("express");
+var router = express.Router();
+var db = require("../queries");
+
+/**
+ * @swagger
+ * definition:
+ *   Puppy:
+ *     properties:
+ *       name:
+ *         type: string
+ *       breed:
+ *         type: string
+ *       age:
+ *         type: integer
+ *       sex:
+ *         type: string
+ */
+/**
+ * @swagger
+ * /api/puppies:
+ *   get:
+ *     tags:
+ *       - Puppies
+ *     description: Returns all puppies
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of puppies
+ *         schema:
+ *           Puppy:
+ *     properties:
+ *       name:
+ *         type: string
+ *       breed:
+ *         type: string
+ *       age:
+ *         type: integer
+ *       sex:
+ *         type: string
+ */
+router.get("/api/puppies", db.getAllPuppies);
+// router.get('/api/puppies/:id', db.getSinglePuppy);
+// router.post('/api/puppies', db.createPuppy);
+// router.put('/api/puppies/:id', db.updatePuppy);
+// router.delete('/api/puppies/:id', db.removePuppy);
+
+// application -------------------------------------------------------------
+router.get("/", function(req, res) {
+  res.render("index", { title: "node-postgres-promises" }); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+module.exports = router;
